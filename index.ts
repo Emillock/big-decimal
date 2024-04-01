3076a198191278375d6510dccedf59c7d3c7c75a
Emil Mirzazada <mirzazadeemil2005@gmail.com>
2024-04-01 16:32:49 +0400

    made BigDecimal constructor such that it can get different data types

--- index.ts
+++ index.ts
@@ -2,9 +2,17 @@ class BigDecimal {
     num1: bigint;
     num2: bigint;
 
-    constructor(num1: bigint, num2: bigint) {
-        this.num1 = num1;
-        this.num2 = num2;
+    constructor(num1: bigint | string | number, num2: bigint | string | number | undefined) {
+
+        if (num2 === undefined) {
+            const numArr = (num1 as string).split(".");
+            this.num1 = BigInt(numArr[0]!);
+            this.num2 = BigInt(numArr[1] ?? 0);
+            return;
+        }
+
+        this.num1 = BigInt(num1);
+        this.num2 = BigInt(num2);
     }
 
     toString() {
