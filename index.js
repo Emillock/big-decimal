1d5decad5d6ac9d8a1e43d02e25f1dd43f655fc8
Emil Mirzazada <mirzazadeemil2005@gmail.com>
2024-04-01 17:23:59 +0400

    added sum method and changed toString to delete useless zeros

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 var BigDecimal = /** @class */ (function () {
     function BigDecimal(num1, num2) {
-        this.num1 = num1;
-        this.num2 = num2;
+        var _a;
+        if (num2 === undefined) {
+            var numArr = num1.split(".");
+            this.num1 = BigInt(numArr[0]);
+            this.num2 = BigInt((_a = numArr[1]) !== null && _a !== void 0 ? _a : 0);
+            return;
+        }
+        this.num1 = BigInt(num1);
+        this.num2 = BigInt(num2);
     }
+    BigDecimal.sum = function (bigDec1, bigDec2) {
+        console.log(bigDec1.toString());
+        var bigDec1Num2Len = bigDec1.num2.toString().length;
+        var bigDec2Num2Len = bigDec2.num2.toString().length;
+        var maxLen = Math.max(bigDec1Num2Len, bigDec2Num2Len);
+        var bigDec1Num2Str = bigDec1.num2.toString().padEnd(maxLen, '0');
+        var bigDec2Num2Str = bigDec2.num2.toString().padEnd(maxLen, '0');
+        console.log(bigDec1Num2Str);
+        console.log(bigDec2Num2Str);
+        var bigDec1Join = BigInt(bigDec1.num1.toString() + bigDec1Num2Str);
+        var bigDec2Join = BigInt(bigDec2.num2.toString() + bigDec2Num2Str);
+        var resBigInt = bigDec1Join + bigDec2Join;
+        var resStr = resBigInt.toString().split('');
+        resStr.splice(resBigInt.toString().length - maxLen, 0, '.');
+        return new BigDecimal(resStr.join(''));
+    };
     BigDecimal.prototype.toString = function () {
-        return "".concat(this.num1.toString(), ".").concat(this.num2.toString());
+        var num1Str = this.num1.toString();
+        var num2Str = this.num2.toString();
+        return "".concat(num1Str).concat(num2Str.replace("0", "").length === 0 ? "" : '.' + num2Str);
     };
     return BigDecimal;
 }());
-throw new Error("111");
-var num = new BigDecimal(BigInt(100), BigInt(1));
-console.log("1" + num.toString());
+var num1 = new BigDecimal(100, 1);
+var num2 = new BigDecimal(100, 2);
+console.log(BigDecimal.sum(num1, num2).toString());
--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ class BigDecimal {
     num1: bigint;
     num2: bigint;
 
-    constructor(num1: bigint | string | number, num2: bigint | string | number | undefined) {
+    constructor(num1: bigint | string | number, num2?: bigint | string | number) {
 
         if (num2 === undefined) {
             const numArr = (num1 as string).split(".");
@@ -15,13 +15,34 @@ class BigDecimal {
         this.num2 = BigInt(num2);
     }
 
+    static sum(bigDec1: BigDecimal, bigDec2: BigDecimal) {
+        
+        const bigDec1Num2Len = bigDec1.num2.toString().length;
+        const bigDec2Num2Len = bigDec2.num2.toString().length;
+        const maxLen = Math.max(bigDec1Num2Len, bigDec2Num2Len);
+
+        const bigDec1Num2Str = bigDec1.num2.toString().padEnd(maxLen, '0');
+        const bigDec2Num2Str = bigDec2.num2.toString().padEnd(maxLen, '0');
+
+        const bigDec1Join = BigInt(bigDec1.num1.toString() + bigDec1Num2Str);
+        const bigDec2Join = BigInt(bigDec2.num1.toString() + bigDec2Num2Str);
+
+        const resBigInt = bigDec1Join + bigDec2Join;
+
+        const resStr = resBigInt.toString().split('');
+        resStr.splice(resBigInt.toString().length - maxLen, 0, '.');
+
+        return new BigDecimal(resStr.join(''));
+    }
+
     toString() {
         const num1Str = this.num1.toString();
         const num2Str = this.num2.toString();
-        return `${num1Str}${num2Str.replace("0", "").length === 0 ? "" : '.' + num2Str}`;
+        return `${num1Str}${num2Str.replace("0", "").length === 0 ? "" : '.' + num2Str.replace(/0+$/,'')}`;
     }
 }
 
-const num = new BigDecimal(BigInt(100), BigInt(1));
+const num1 = new BigDecimal(100, 1000000000000000000000000000n);
+const num2 = new BigDecimal(100, 2);
 
-console.log(num.toString());
+console.log(BigDecimal.sum(num1, num2).toString());
