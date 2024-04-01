3e8f2a7d341c2a364b9e15d13beae2d4ddfd1bab
Emil Mirzazada <mirzazadeemil2005@gmail.com>
2024-04-01 21:59:15 +0400

    added isNegative field for handling negative numbers

--- index.ts
+++ index.ts
@@ -1,18 +1,24 @@
 class BigDecimal {
     num1: bigint;
     num2: bigint;
+    isNegative: boolean;
 
     constructor(num1: bigint | string | number, num2?: bigint | string | number) {
-
+        this.isNegative = num1.toString().includes('-');
         if (num2 === undefined) {
             const numArr = (num1 as string).split(".");
-            this.num1 = BigInt(numArr[0]!);
-            this.num2 = BigInt(numArr[1] ?? 0);
+
+            this.num1 = BigDecimal.bigIntAbs(BigInt(numArr[0] === "-" ? 0 : numArr[0]!));
+            this.num2 = BigDecimal.bigIntAbs(BigInt(numArr[1] ?? 0));
             return;
         }
 
-        this.num1 = BigInt(num1);
-        this.num2 = BigInt(num2);
+        this.num1 = BigDecimal.bigIntAbs(BigInt(num1));
+        this.num2 = BigDecimal.bigIntAbs(BigInt(num2));
+    }
+
+    private static bigIntAbs(bigInt: bigint) {
+        return (bigInt < 0n) ? -bigInt : bigInt;
     }
 
     private static bigDecJoin(bigDec1: BigDecimal, bigDec2: BigDecimal) {
@@ -23,8 +29,8 @@ class BigDecimal {
         const bigDec1Num2Str = bigDec1.num2.toString().padEnd(maxLen, '0');
         const bigDec2Num2Str = bigDec2.num2.toString().padEnd(maxLen, '0');
 
-        const bigDec1Join = BigInt(bigDec1.num1.toString() + bigDec1Num2Str);
-        const bigDec2Join = BigInt(bigDec2.num1.toString() + bigDec2Num2Str);
+        const bigDec1Join = BigInt((bigDec1.isNegative ? "-" : "") + bigDec1.num1.toString() + bigDec1Num2Str);
+        const bigDec2Join = BigInt((bigDec2.isNegative ? "-" : "") + bigDec2.num1.toString() + bigDec2Num2Str);
 
         return { bigDecJoinArr: [bigDec1Join, bigDec2Join], maxLen };
     }
@@ -60,11 +66,11 @@ class BigDecimal {
     toString() {
         const num1Str = this.num1.toString();
         const num2Str = this.num2.toString();
-        return `${num1Str}${num2Str.replace("0", "").length === 0 ? "" : '.' + num2Str.replace(/0+$/, '')}`;
+        return `${this.isNegative ? "-" : ""}${num1Str}${num2Str.replace("0", "").length === 0 ? "" : '.' + num2Str.replace(/0+$/, '')}`;
     }
 }
 
-const num1 = new BigDecimal(100, 1000000000000000000000000000n);
+const num1 = new BigDecimal(100, 1000000000000000000000000001n);
 const num2 = new BigDecimal(100, 2);
 
 console.log(BigDecimal.sum(num1, num2).toString());
